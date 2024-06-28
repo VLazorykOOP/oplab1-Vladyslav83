2e03610c83d719e41863bd1809a34e394ad979da
Vladyslav83 <wight3310@gmail.com>
2024-06-28 14:33:07 +0300

    done

---
+++ test_for24/Source.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <string>
+#include <unordered_map>
+
+// Функції Qnk, Rnk, Qnr, Gnk, Wnk, U, T, Tfun, та інші
+double Qnr(double x, double y);
+double Qnk(double x, double y);
+double Rnk(double x, double y, double z);
+double Gnk(double x, double y, double z);
+double Wnk(double x, double y);
+double U(double x, const std::unordered_map<double, double>& data);
+double T(double x, const std::unordered_map<double, double>& data);
+double Tfun(double u, double v, const std::string& text, const std::unordered_map<std::string, double>& dat3);
+
+double func(double u, double v, const std::string& text, const std::unordered_map<std::string, double>& dat3) {
+    if (u < 0.5 && v < 0.5) {
+        return Tfun(u, v, text, dat3);
+    }
+    else if (u < 0.5 && v >= 0.5) {
+        return Tfun(0, v, text, dat3);
+    }
+    else if (u >= 0.5 && v < 0.5) {
+        return Tfun(u, 0, text, dat3);
+    }
+    else {
+        return Tfun(0, 0, "", dat3);
+    }
+}
+
+int main() {
+    // Зчитування файлів dat1.dat, dat2.dat та dat3.dat
+    std::unordered_map<double, double> dat1, dat2;
+    std::unordered_map<std::string, double> dat3;
+
+    std::ifstream file1("dat1.dat");
+    if (file1.is_open()) {
+        double x, y;
+        while (file1 >> x >> y) {
+            dat1[x] = y;
+        }
+        file1.close();
+    }
+
+    std::ifstream file2("dat2.dat");
+    if (file2.is_open()) {
+        double x, y;
+        while (file2 >> x >> y) {
+            dat2[x] = y;
+        }
+        file2.close();
+    }
+
+    std::ifstream file3("dat3.dat");
+    if (file3.is_open()) {
+        std::string key;
+        double value;
+        while (file3 >> key >> value) {
+            dat3[key] = value;
+        }
+        file3.close();
+    }
+
+    // Введення x, y, z та text
+    double x, y, z;
+    std::string text;
+    std::cin >> x >> y >> z;
+    std::cin.ignore(); // Ігноруємо залишок рядка після чисел
+    std::getline(std::cin, text);
+
+    // Обчислення функції
+    double u, v, gold;
+    u = func1(x, y, z);
+    v = func2(x, y, z);
+
+    if (/* умови алгоритму 1 */) {
+        // обчислення за алгоритмом 1
+    }
+    else if (/* умови алгоритму 2 */) {
+        // обчислення за алгоритмом 2
+    }
+    else if (/* умови алгоритму 3 */) {
+        // обчислення за алгоритмом 3
+    }
+    else if (/* умови алгоритму 4 */) {
+        // обчислення за алгоритмом 4
+    }
+
+    // Виклик функції func для отримання результату
+    double result = func(u, v, text, dat3);
+
+    // Виведення результату
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
+// Реалізація функцій
+double Qnr(double x, double y) {
+    if (x != 0 && y != 0) {
+        if (x > 0 && y <= 0) {
+            return 1.0 / (10 * y - 2 * x);
+        }
+        else if (x > 0 && y > 0) {
+            return std::log(10 * y - 3 * x) * 10;
+        }
+        else if (x <= 0 && y <= 0) {
+            return 3 * x * y;
+        }
+    }
+    return 1.0;
+}
+
+double Qnk(double x, double y) {
+    return 10 - 5 * Qnr(2 * x, 2 * y) + 3.75 * Qnr(y, x);
+}
+
+double Rnk(double x, double y, double z) {
+    return Qnk(x, z) + Qnk(z, y) + Qnk(y, x);
+}
+
+double Gnk(double x, double y, double z) {
+    return Wnk(x, z) + Wnk(z, y) + Wnk(y, x);
+}
+
+double Wnk(double x, double y) {
+    return 10 - 5 * Qnr(2 * x, 2 * y) + 3.75 * Qnr(y, x);
+}
+
+double U(double x, const std::unordered_map<double, double>& data) {
+    auto it = data.find(x);
+    if (it != data.end()) {
+        return it->second;
+    }
+    return 0.0; // Значення за замовчуванням
+}
+
+double T(double x, const std::unordered_map<double, double>& data) {
+    auto it = data.find(x);
+    if (it != data.end()) {
+        return it->second;
+    }
+    return 0.0; // Значення за замовчуванням
+}
+
+double Tfun(double u, double v, const std::string& text, const std::unordered_map<std::string, double>& dat3) {
+    double r = 0.0;
+    auto it = dat3.find(text);
+    if (it != dat3.end()) {
+        r = it->second;
+    }
+    return std::sqrt((u - v) * (u - v) + r * r * r);
+}
+
+double func1(double x, double y, double z) {
+    return Qnk(x, y) + Rnk(x, y, z);
+}
+
+double func2(double x, double y, double z) {
+    return Gnk(x, y, z);
+}
